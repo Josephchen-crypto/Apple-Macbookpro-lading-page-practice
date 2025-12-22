71b8a2b61777954a8ba7d5a731325c3b1808679a
chendeji <781571323@qq.com>
2025-12-22 15:11:02 +0800

    add gsap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ProductViewer from "./components/ProductViewer";
 
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const App = () => {
   return (
     <main>
