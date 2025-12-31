9ba7df5ce4c24e2da805aa89f8f492ed6a45f6c4
chendeji <781571323@qq.com>
2025-12-31 09:58:25 +0800

    finish Features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,16 +1,75 @@
 import { Canvas } from "@react-three/fiber";
 import StudioLights from "./three/StudioLights";
-import { features } from "../constants";
+import { features, featureSequence } from "../constants";
 import clsx from "clsx";
 import { useRef } from "react";
 import { Suspense } from "react";
 import { Html } from "@react-three/drei";
 import MacbookModel from "./models/MacbookModel";
 import { useMediaQuery } from "react-responsive";
+import useMacbookStore from "../store";
+import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const ModelScroll = () => {
   const groupRef = useRef(null);
   const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const { setTexture } = useMacbookStore();
+
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const v = document.createElement("video");
+      Object.assign(v, {
+        src: feature.videoPath,
+        muted: true,
+        playInline: true,
+        preload: "auto",
+        crossOrigin: "anonymous",
+      });
+      v.load();
+    });
+  }, []);
+
+  useGSAP(() => {
+    const modelTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+      },
+    });
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top center",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+    if (groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, {
+        y: Math.PI * 2,
+        ease: "power1.inOut",
+      });
+    }
+
+    // 内容根据屏幕滚动动画
+    timeLine
+      .call(() => setTexture("/videos/feature-1.mp4"))
+      .to(".box1", { opacity: 1, y: 0, delay: 1 })
+      .call(() => setTexture("/videos/feature-2.mp4"))
+      .to(".box2", { opacity: 1, y: 0 })
+      .call(() => setTexture("/videos/feature-3.mp4"))
+      .to(".box3", { opacity: 1, y: 0 })
+      .call(() => setTexture("/videos/feature-4.mp4"))
+      .to(".box4", { opacity: 1, y: 0 })
+      .call(() => setTexture("/videos/feature-5.mp4"))
+      .to(".box5", { opacity: 1, y: 0 });
+  }, []);
+
   return (
     <group ref={groupRef}>
       <Suspense
@@ -30,7 +89,7 @@ const Features = () => {
   return (
     <section id="features">
       <h2>See it all in a new light.</h2>
-      <Canvas id="f-canvas" camare={{}}>
+      <Canvas id="f-canvas" className="border border-red-300" camare={{}}>
         <StudioLights />
         <ambientLight intensity={0.5} />
         <ModelScroll />
@@ -41,7 +100,11 @@ const Features = () => {
             key={feature.id}
             className={clsx("box", `box${index + 1}`, feature.styles)}
           >
-            {feature.text}
+            <img src={feature.icon} alt={feature.highlight} />
+            <p>
+              <span className="text-white">{feature.highlight}</span>
+              {feature.text}
+            </p>
           </div>
         ))}
       </div>
