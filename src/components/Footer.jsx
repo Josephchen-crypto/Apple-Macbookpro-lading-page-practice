21e4cc2e7f411f845c75f38bf933967ea0a108c8
chendeji <781571323@qq.com>
2026-01-06 14:12:54 +0800

    implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop:
+          <span>Find an Apple Store</span> or <span>other retailer</span> near
+          you. Or call 000800 040 1966.
+        </p>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map((link) => (
+            <li key={link.label}>
+              <a href={link.link}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
